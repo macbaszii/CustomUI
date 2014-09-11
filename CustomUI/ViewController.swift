926fac3d1fd270d88d58ad6e3bec413856fc6011
Bas <bas@oozou.com>
2014-09-11 11:25:17 +0700

    Initial Commit

---
+++ CustomUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomUI
+//
+//  Created by Kiattisak Anoochitarom on 9/11/2557 BE.
+//  Copyright (c) 2557 Kiattisak Anoochitarom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
